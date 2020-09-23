8b3969865b638cf1d07ff5033604c8a9b4b09964
MissSunflora <nesabertanico@yahoo.com.ph>
2020-09-23 17:40:30 -0400

    Disable color

--- src/nesabyte_bapples/Cleaner.java
+++ src/nesabyte_bapples/Cleaner.java
@@ -1,19 +1,5 @@
 package nesabyte_bapples;
 
-/*import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;*/
-
 import java.net.*;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -24,15 +10,10 @@ import java.io.*;
 
 public class Cleaner {
 	
-	public static final String ANSI_RESET = "\u001B[0m";
-	public static final String ANSI_BLACK = "\u001B[30m";
-	public static final String ANSI_RED = "\u001B[31m";
-	public static final String ANSI_GREEN = "\u001B[32m";
-	public static final String ANSI_YELLOW = "\u001B[33m";
-	public static final String ANSI_BLUE = "\u001B[34m";
-	public static final String ANSI_PURPLE = "\u001B[35m";
-	public static final String ANSI_CYAN = "\u001B[36m";
-	public static final String ANSI_GRAY = "\u001b[0m";
+	public static final String ANSI_RESET = "";// "\u001B[0m";
+	public static final String ANSI_RED = "";// "\u001B[31m";
+	public static final String ANSI_GREEN = "";// "\u001B[32m";
+	public static final String ANSI_GRAY = "";// "\u001b[0m";
 	
 	/**
 	 * Demonstrate processing a single provided argument.
